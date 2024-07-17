1ab86de949d0b4373d8aab518100ce37dd29732f
Documenter.jl <documenter@juliadocs.github.io>
2024-07-17 20:08:35 +0000

    build based on e04ab00

--- dev/generated/eight_schools_centered_model/src/create_inputs.jl
+++ dev/generated/eight_schools_centered_model/src/create_inputs.jl
@@ -1,6 +1,6 @@
 run(`git clone https://github.com/Julia-Tempering/InferenceReport.jl`)
 cd("InferenceReport.jl")
-run(`git checkout 2c20e17747ab70dce82b450106cc868099c6a48b`)
+run(`git checkout e04ab00bc0d5c283b7552c59bd8850ff1c87187b`)
 
 using Pkg 
 Pkg.activate(".")
