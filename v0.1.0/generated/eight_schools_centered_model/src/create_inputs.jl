4c06ef525ec4b6f40cea842ca1ab162b0d019286
Documenter.jl <documenter@juliadocs.github.io>
2024-09-20 19:12:17 +0000

    build based on f26f124

---
+++ v0.1.0/generated/eight_schools_centered_model/src/create_inputs.jl
@@ -0,0 +1,11 @@
+run(`git clone https://github.com/Julia-Tempering/InferenceReport.jl`)
+cd("InferenceReport.jl")
+run(`git checkout f26f12430f67a2c8c64988c8c8c1d80696a7d7fd`)
+
+using Pkg 
+Pkg.activate(".")
+Pkg.instantiate()
+ 
+
+using Pigeons
+inputs = Inputs(; target = Pigeons.stan_eight_schools(), variational = GaussianReference(first_tuning_round = 5), n_rounds = 10, record = [traces; round_trip; record_default()])
